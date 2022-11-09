cc83fecbf9f6ca6a6a2666e5a70eb36aa3c02e81
javierhol <javierholguin596@gmail.com>
2022-11-08 19:32:51 -0500

    Create Api with SpringBoot and Mysql

---
+++ ApiRest/src/main/java/com/jhv/ApiRest/controller/ApiController.java
@@ -0,0 +1,18 @@
+package com.jhv.ApiRest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/")
+public class ApiController {
+
+    @GetMapping("example")
+    public ResponseEntity<Object> index() {
+        return ResponseEntity.ok("OK");
+    }
+}
